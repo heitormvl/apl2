d25552bced9017cbb665440ea6104079ea55d760
heitormvl <heitormvl12@gmail.com>
2023-05-17 20:31:47 -0300

    Implementação do Node

--- src/Node.java
+++ src/Node.java
@@ -1,6 +1,11 @@
 // arquivo: src/apl2/Node.java
 
-// TODO: Colocar a identificação dos(as) integrantes aqui.
+// Programa por:
+//  Heitor Maciel (32251661)
+//  Davi Rodrigues (32266960)
+//  Vinícius Magno (32223201)
+//  Gabriel Braum (32224532)
+//  Roberto Rinco (32269471)
 
 // -- A classe Node (que pertence ao pacote apl2) deve conter os atributos que
 // representam a nova versão dos dados de uma pessoa, conforme descrito no
@@ -13,7 +18,67 @@
 // {...}, retornando uma string com os valores dos atributos da classe.
 
 public class Node {
-	
-	// TODO: Implementar a classe conforme o enunciado da atividade Apl2.
-	
+
+    private String id;
+    private String nome;
+    private float nota;
+    private Node next;
+    private Node prev;
+
+    public Node() {
+        this("", "", 0, null, null);
+    }
+
+    public Node(String id, String nome, float nota, Node next, Node prev) {
+        this.id = id;
+        this.nome = nome;
+        this.nota = nota;
+        this.next = next;
+        this.prev = prev;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getNota() {
+        return nota;
+    }
+
+    public void setNota(float nota) {
+        this.nota = nota;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    @Override
+    public String toString() {
+        return "[dados: (" + id + ";" + nome + ";" + nota + ") | next: " + next + " | prev: " + prev + "]";
+    }
 }
